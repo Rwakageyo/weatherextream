81068dafdd66117d993ee1f21b93cd33343393cd
NIZEYIMANA RWAKAGEYO Valens <32904514+Rwakageyo@users.noreply.github.com>
2022-11-16 23:14:51 +0200

    Update Maximum temperature.py

--- pages/Maximum temperature.py
+++ pages/Maximum temperature.py
@@ -15,10 +15,10 @@ image = Image.open('logos.png')
 
 #st.image(image, caption=None, use_column_width='auto')
 
-col1=st.columns([1])
+##col1=st.columns([1])
 
 ##with col1:
-st.image(image,width=60,use_column_width='auto')
+##st.image(image,width=60,use_column_width='auto')
 
 
 hide_menu_style = """
