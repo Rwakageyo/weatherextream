d03d3db718538bd2ccd430ef5a3fcedaaedc4a8a
NIZEYIMANA RWAKAGEYO Valens <32904514+Rwakageyo@users.noreply.github.com>
2022-11-16 13:18:06 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 ##image = Image.open('logos.png')
 ##st.image(image)
   
-st.title("Temperature Minimum  Extrem Dashboard")
+st.title("Rainfall Extream Dashboard")
 st.markdown("The dashboard will help a researcher to get to know \
 more about the given datasets and it's output")
 
