1ae8700c3fb5ff968c2a4c06734cb986defa0ed2
NIZEYIMANA RWAKAGEYO Valens <32904514+Rwakageyo@users.noreply.github.com>
2022-11-16 15:18:30 +0200

    Update Rainfall Extream.py

--- pages/Rainfall Extream.py
+++ pages/Rainfall Extream.py
@@ -4,11 +4,11 @@ import numpy as np
 import plotly.graph_objects as go
 ##import hydralit_components as hc
 
-##from PIL import Image
+#from PIL import Image
 ##image = Image.open('logos.png')
 ##st.image(image)
   
-st.title("Temperature Minimum  Extrem Dashboard")
+st.title("Rainfall Extream Dashboard")
 st.markdown("The dashboard will help a researcher to get to know \
 more about the given datasets and it's output")
 
@@ -41,9 +41,9 @@ chart_visual = st.sidebar.selectbox('Select Charts/Plot type',
   
 st.sidebar.checkbox("Show Analysis by Extreme Status", True, key = 1)
 selected_status = st.sidebar.selectbox('Select Extreme Status',
-                                       options = ['Station_name', 
-                                                  'Value','District','Dates', 
-                                                  'Province'])
+                                       options = [' Station_name', 
+                                                  'Value_mm','Province','District', 
+                                                  'Date'])
 
 fig = go.Figure()
   
@@ -51,56 +51,56 @@ if chart_visual == 'Line Chart':
     if selected_status == 'Station_name':
         fig.add_trace(go.Scatter(x = data.Station_name, y = data.Value_mm,
                                  mode = 'lines',
-                                 name = 'Station_name'))
-    if selected_status == 'Station_name':
-        fig.add_trace(go.Scatter(x = data.District, y = data.Value_mm,
-                                 mode = 'lines', name = 'Value'))
+                                 name = 'Value_mm'))
+    if selected_status == 'District':
+        fig.add_trace(go.Scatter(x = data.Station_name, y = data.Value_mm,
+                                 mode = 'lines', name = 'Value_mm'))
     if selected_status == 'Station_name':
         fig.add_trace(go.Scatter(x = data.District, y = data.Value_mm,
                                  mode = 'lines',
-                                 name = 'Value'))
-    if selected_status == 'Value_mm': 
-        fig.add_trace(go.Scatter(x=data.District, y=data.Province,
+                                 name = 'Value_mm'))
+    if selected_status == 'Province': 
+        fig.add_trace(go.Scatter(x=data.Station_name, y=data.Value_mm,
                                  mode='lines',
                                  name="Province"))
   
 elif chart_visual == 'Bar Chart':
     if selected_status == 'Station_name':
-        fig.add_trace(go.Bar(x=data.District, y=data.Value,
-                             name='Value'))
-    if selected_status == 'Element':
-        fig.add_trace(go.Bar(x=data.District, y=data.Value,
+        fig.add_trace(go.Bar(x=data.Station_name, y=data.Value_mm,
+                             name='Value_mm'))
+    if selected_status == 'District':
+        fig.add_trace(go.Bar(x=data.Station_name, y=data.Value_mm,
                              name='Station_name'))
     if selected_status == 'Province':
-        fig.add_trace(go.Bar(x=data.District, y=data.Value,
-                             name='Station_name'))
-    if selected_status == 'Station_name':
         fig.add_trace(go.Bar(x=data.District, y=data.Value_mm,
-                             name="Value_mm"))
+                             name='Station_name'))
+    if selected_status == 'Dates':
+        fig.add_trace(go.Bar(x=data.Station_name, y=data.Value_mm,
+                             name="Unknown"))
   
 elif chart_visual == 'Bubble Chart':
     if selected_status == 'Station_name':
-        fig.add_trace(go.Scatter(x=data.District, 
+        fig.add_trace(go.Scatter(x=data.Station_name, 
                                  y=data.Value_mm,
                                  mode='markers',
                                  marker_size=[10,20,30,40, 60, 80, 60, 40, 50,60,70,100,110,120,130,140,150],
                                  name='Station_name'))
           
-    if selected_status == 'District':
-        fig.add_trace(go.Scatter(x=data.District, y=data.Station_name,
+    if selected_status == 'Province':
+        fig.add_trace(go.Scatter(x=data.Station_name, y=data.Value_mm,
                                  mode='markers', 
                                  marker_size=[10,20,30,40, 60, 80, 60, 40, 50,60,70,100,110,120,130,140,150],
-                                 name='Value_mm'))
+                                 name='Province'))
           
-    if selected_status == 'Station':
-        fig.add_trace(go.Scatter(x=data.District,
-                                 y=data.Station,
+    if selected_status == 'District':
+        fig.add_trace(go.Scatter(x=data.Station_name,
+                                 y=data.Value_mm,
                                  mode='markers', 
                                  marker_size=[10,20,30,40, 60, 80, 60, 40, 50,60,70,100,110,120,130,140,150],
-                                 name = 'Station'))
+                                 name = 'Station_name'))
     if selected_status == 'Station_name':
         fig.add_trace(go.Scatter(x=data.District,
-                                 y=data.Dates,
+                                 y=data.Value_mm,
                                  mode='markers',
                                  marker_size=[10,20,30,40, 60, 80, 60, 40, 50,60,70,100,110,120,130,140,150],
                                  name="Value_mm"))
