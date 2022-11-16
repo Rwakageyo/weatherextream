be0d09b78283824f333c0dab63cb3f16f00d286f
NIZEYIMANA RWAKAGEYO Valens <32904514+Rwakageyo@users.noreply.github.com>
2022-11-16 22:33:21 +0200

    Update Maximum temperature.py

--- pages/Maximum temperature.py
+++ pages/Maximum temperature.py
@@ -22,7 +22,12 @@ import plotly.graph_objects as go
     ##st.image(image2,width=360,use_column_width='auto')
 
 
-
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.title(' Rwanda Climate Extremes Breaking Records')
 
