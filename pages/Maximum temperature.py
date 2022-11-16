0ca336734249e13cffa7f7ef5a06c5de2c4cf12b
NIZEYIMANA RWAKAGEYO Valens <32904514+Rwakageyo@users.noreply.github.com>
2022-11-16 22:04:07 +0200

    Update Maximum temperature.py

--- pages/Maximum temperature.py
+++ pages/Maximum temperature.py
@@ -54,6 +54,83 @@ District_sidebar = st.sidebar.selectbox('Select a District:', data2)
 dataps=data.loc[data.District==District_sidebar]
 dataps
 
+#District_select = data['District'].drop_duplicates()
+
+District_sidebar = st.sidebar.selectbox('Select a District:', data2)
+dataps=data.loc[data.District==District_sidebar]
+dataps
+
+st.sidebar.title("Select Visual Charts")
+  
+chart_visual = st.sidebar.selectbox('Select Charts/Plot type', 
+                                    ('Line Chart', 'Bar Chart', 'Bubble Chart'))
+  
+st.sidebar.checkbox("Show Analysis by Extreme Status", True, key = 1)
+selected_status = st.sidebar.selectbox('Select Extreme Status',
+                                       options = ['Province', 
+                                                  'District','Station_name','Value_mm', 
+                                                  'Date'])
+
 fig = go.Figure()
+  
+if chart_visual == 'Line Chart':
+    if selected_status == 'Station_name':
+        fig.add_trace(go.Scatter(x = data.District, y = data.Value_mm,
+                                 mode = 'lines',
+                                 name = 'Station_name'))
+    if selected_status == 'District':
+        fig.add_trace(go.Scatter(x = data.Station_name, y = data.Value_mm,
+                                 mode = 'lines', name = 'District'))
+    if selected_status == 'Station_name':
+        fig.add_trace(go.Scatter(x = data.District, y = data.Value_mm,
+                                 mode = 'lines',
+                                 name = 'Station_name'))
+    if selected_status == 'Province': 
+        fig.add_trace(go.Scatter(x=data.Station_name, y=data.Value_mm,
+                                 mode='lines',
+                                 name="Province"))
+  
+elif chart_visual == 'Bar Chart':
+    if selected_status == 'Station_name':
+        fig.add_trace(go.Bar(x=data.Station_name, y=data.Value_mm,
+                             name='Station_name'))
+    if selected_status == 'District':
+        fig.add_trace(go.Bar(x=data.Station_name, y=data.Value_mm,
+                             name='District'))
+    if selected_status == 'Province':
+        fig.add_trace(go.Bar(x=data.Station_name, y=data.Value_mm,
+                             name='Province'))
+    if selected_status == 'Dates':
+        fig.add_trace(go.Bar(x=data.Station_name, y=data.Value_mm,
+                             name="Dates"))
+  
+elif chart_visual == 'Bubble Chart':
+    if selected_status == 'Station_name':
+        fig.add_trace(go.Scatter(x=data.Station_name, 
+                                 y=data.Value_mm,
+                                 mode='markers',
+                                 marker_size=[10,20,30,40, 60, 80, 60, 40, 50,60,70,100,110,120,130,140,150],
+                                 name='Station_name'))
+          
+    if selected_status == 'Province':
+        fig.add_trace(go.Scatter(x=data.Station_name, y=data.Value_mm,
+                                 mode='markers', 
+                                 marker_size=[10,20,30,40, 60, 80, 60, 40, 50,60,70,100,110,120,130,140,150],
+                                 name='Province'))
+          
+    if selected_status == 'District':
+        fig.add_trace(go.Scatter(x=data.Station_name,
+                                 y=data.Value_mm,
+                                 mode='markers', 
+                                 marker_size=[10,20,30,40, 60, 80, 60, 40, 50,60,70,100,110,120,130,140,150],
+                                 name = 'District'))
+    if selected_status == 'Station_name':
+        fig.add_trace(go.Scatter(x=data.District,
+                                 y=data.Value_mm,
+                                 mode='markers',
+                                 marker_size=[10,20,30,40, 60, 80, 60, 40, 50,60,70,100,110,120,130,140,150],
+                                 name="Station_name"))
+  
+st.plotly_chart(fig, use_container_width=True)
 
 
