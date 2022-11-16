be66ff9ad8aa6e2b388e0558e46a9f007e840e8f
NIZEYIMANA RWAKAGEYO Valens <32904514+Rwakageyo@users.noreply.github.com>
2022-11-16 23:47:37 +0200

    Update Minimum temperature.py

--- pages/Minimum temperature.py
+++ pages/Minimum temperature.py
@@ -12,6 +12,28 @@ st.set_page_config(
     initial_sidebar_state="expanded",
     
 )
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image:('logos.png');
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 image = Image.open('Govlogo.png')
 image2 = Image.open('logos.png')
