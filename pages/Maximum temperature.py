a8440e172f1e91fc21af9c3bd3f60dc45533c334
NIZEYIMANA RWAKAGEYO Valens <32904514+Rwakageyo@users.noreply.github.com>
2022-11-16 23:06:36 +0200

    Update Maximum temperature.py

--- pages/Maximum temperature.py
+++ pages/Maximum temperature.py
@@ -15,10 +15,9 @@ st.image(image)
 
 #st.image(image, caption=None, use_column_width='auto')
 
-##col1,col2=st.columns([2,2])
-
-with col1:
-      st.image(image,width=50,use_column_width='auto')
+col1,col2=st.columns([2,2])
+      
+      st.image(image,width=50 ,height=20 ,use_column_width='auto')
 ##with col2:
     ##st.image(image2,width=360,use_column_width='auto')
 
